7c5115cca2370e51465078332751c2ccb5ed1d16
PedroF3rreira <pedro-edu19@outlook.com>
2022-04-06 20:38:24 -0300

    criando classes para peças e tabuleiro do xadrez

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		Board b = new Board(8,8);
+		System.out.println(b.piece(1, 1));
 	}
 
 }
